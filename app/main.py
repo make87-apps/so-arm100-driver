f4182788ca58b0ec103b6a7b94b74d627cf66a63
user <make87>
2025-07-18 13:23:46 +0000

    lerobot code and interface update

--- app/main.py
+++ app/main.py
@@ -1,30 +1,37 @@
 from datetime import datetime, timezone
 
-from make87_messages.core.header_pb2 import Header
-from make87_messages.text.text_plain_pb2 import PlainText
 import make87
+import logging
+from app.so100_autodetect import start_so100_robot_client
 
+logger = logging.getLogger(__name__)
 
 def main():
-    make87.initialize()
-    endpoint = make87.get_provider(
-        name="EXAMPLE_ENDPOINT", requester_message_type=PlainText, provider_message_type=PlainText
+    config = make87.config.load_config_from_env()
+    lerobot_interface = config.interfaces.get("lerobot")
+    if not lerobot_interface:
+        logger.warning("No lerobot found in interface configuration.")
+        return
+
+    robotclient = lerobot_interface.clients.get("robotclient")
+    if not robotclient:
+        logger.warning("No robotclient client found in configuration.")
+        return
+
+    
+    robot_index = make87.config.get_config_value(config, "robot_index", default=0, converter=int)
+    fps = make87.config.get_config_value(config, "fps", default=10, converter=int)
+    actions_per_chunk = make87.config.get_config_value(config, "actions_per_chunk", default=10, converter=int)
+    environment_dt = make87.config.get_config_value(config, "environment_dt", default=0.1, converter=float)
+
+    server_address = f"{robotclient.vpn_ip}:{robotclient.vpn_port}"
+    start_so100_robot_client(
+        index=robot_index,
+        server_address=server_address,
+        fps=fps,
+        actions_per_chunk=actions_per_chunk,
+        environment_dt=environment_dt
     )
-
-    def callback(message: PlainText) -> PlainText:
-        received_dt = datetime.now(tz=timezone.utc)
-        publish_dt = message.header.timestamp.ToDatetime().replace(tzinfo=timezone.utc)
-        print(
-            f"Received message '{message.body}'. Sent at {publish_dt}. Received at {received_dt}. Took {(received_dt - publish_dt).total_seconds()} seconds."
-        )
-
-        return PlainText(
-            header=make87.header_from_message(Header, message=message, append_entity_path="response"),
-            body=message.body[::-1],
-        )
-
-    endpoint.provide(callback)
-    make87.loop()
 
 
 if __name__ == "__main__":
