a8cf5f2b381a602b6e8d7d8675821392ff43b919
PhillipThomas1992 <phillipthomas@live.de>
2025-07-23 15:37:27 +0200

    added second camera option

--- app/main.py
+++ app/main.py
@@ -34,31 +34,52 @@ def main():
     actions_per_chunk = make87.config.get_config_value(config, "actions_per_chunk", default=10, converter=int)
     pretrained_name_or_path = make87.config.get_config_value(config, "pretrained_name_or_path",
                                                              default="helper2424/smolvla_rtx_movet")
+    camera_1_name = make87.config.get_config_value(config, "camera_1_name", default="front")
+    camera_2_name = make87.config.get_config_value(config, "camera_2_name", default="wrist")
 
     manager = make87.peripherals.manager.PeripheralManager(make87_config=config)
-    camera: CameraPeripheral = manager.get_peripheral_by_name("CAMERA")
+    try:
+        camera_1: CameraPeripheral = manager.get_peripheral_by_name("CAMERA_1")
+    except KeyError:
+        camera_1 = None
+
+    try:
+        camera_2: CameraPeripheral = manager.get_peripheral_by_name("CAMERA_2")
+    except KeyError:
+        camera_2 = None
 
     zenoh_interface = ZenohInterface(name="zenoh-client", make87_config=config)
     action_publisher = zenoh_interface.get_publisher(name="AGENT_LOGS")
-    camera_publisher = zenoh_interface.get_publisher(name="CAMERA_IMAGE")
+    camera_1_publisher = zenoh_interface.get_publisher(name="CAMERA_1_IMAGE")
+    camera_2_publisher = zenoh_interface.get_publisher(name="CAMERA_2_IMAGE")
     agent_chat_provider = zenoh_interface.get_queryable(name="AGENT_CHAT")
 
     server_address = f"{robotclient.vpn_ip}:{robotclient.vpn_port}"
 
-    cam_name = "side"
 
     def on_observation_callback(observation: Observation):
         if not observation:
             return
-        frame = observation["side"]
-        try:
-            ret, jpeg = cv2.imencode(".jpg", frame[...,::-1], [cv2.IMWRITE_JPEG_QUALITY, 80])
-            msg = ImageJPEG(data=jpeg.tobytes())
-            message_encoded = m87.encodings.ProtobufEncoder(message_type=ImageJPEG).encode(msg)
-            camera_publisher.put(message_encoded)
-        except Exception as e:
-            logger.error(f"Error sending image: {e}")
-            return
+        if camera_1_name in observation:
+            frame = observation[camera_1_name]
+            try:
+                ret, jpeg = cv2.imencode(".jpg", frame[...,::-1], [cv2.IMWRITE_JPEG_QUALITY, 95])
+                msg = ImageJPEG(data=jpeg.tobytes())
+                message_encoded = m87.encodings.ProtobufEncoder(message_type=ImageJPEG).encode(msg)
+                camera_1_publisher.put(message_encoded)
+            except Exception as e:
+                logger.error(f"Error sending image: {e}")
+                return
+        if camera_2_name in observation:
+            frame = observation[camera_2_name]
+            try:
+                ret, jpeg = cv2.imencode(".jpg", frame[...,::-1], [cv2.IMWRITE_JPEG_QUALITY, 95])
+                msg = ImageJPEG(data=jpeg.tobytes())
+                message_encoded = m87.encodings.ProtobufEncoder(message_type=ImageJPEG).encode(msg)
+                camera_2_publisher.put(message_encoded)
+            except Exception as e:
+                logger.error(f"Error sending image: {e}")
+                return
 
     def on_action_callback(action):
         pass
@@ -72,15 +93,19 @@ def main():
             logger.error(f"Error receiving task: {e}")
             return ""
 
+    camera_paths = dict()
+    if camera_1:
+        camera_paths[camera_1_name] = camera_1.reference
+    if camera_2:
+        camera_paths[camera_2_name] = camera_2.reference
+
     robot_config = get_so100_config(
         server_address=server_address,
         fps=fps,
         actions_per_chunk=actions_per_chunk,
-        pretrained_name_or_path="TensorTemplar/smolvla-singlecam2",
+        pretrained_name_or_path=pretrained_name_or_path,
         index=robot_index,
-        camera_paths={
-            cam_name: camera.reference
-        },
+        camera_paths=camera_paths,
     )
 
     CustomRobotClient.run_robot_client(
